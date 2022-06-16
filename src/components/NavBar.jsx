13733df202470a86797f842f9c93c8fd26bc7327
charliebrown1221 <chaimbrown1@yahoo.com>
2022-06-16 14:56:37 -0400

    added style to heading(last project commit)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import "./Post/Post.css"
 
 const NaveBar = (props) => {
     return ( 
-        <header> Social Feed</header>
+        <header> <span className='nav-textF'>Social</span><span className='nav-textL'>Feed</span> </header>
         
      );
 }
